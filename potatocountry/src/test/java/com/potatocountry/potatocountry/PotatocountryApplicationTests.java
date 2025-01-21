42fa06874d0a565426f738bd1773c0a9989fb512
seykim <kksy0917@gmail.com>
2025-01-21 18:35:34 +0900

    ✨ [Feature] Init Project

--- potatocountry/src/test/java/com/potatocountry/potatocountry/PotatocountryApplicationTests.java
+++ potatocountry/src/test/java/com/potatocountry/potatocountry/PotatocountryApplicationTests.java
@@ -2,9 +2,7 @@ package com.potatocountry.potatocountry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class PotatocountryApplicationTests {
 
