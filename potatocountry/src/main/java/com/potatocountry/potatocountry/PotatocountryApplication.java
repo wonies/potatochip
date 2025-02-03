e3939e1b75a009ac52b8c6ea4198469be1700706
taehyeon3 <117972735+taehyeon3@users.noreply.github.com>
2025-02-03 17:42:39 +0900

    ✅ [Test] codecov 적용 (#6)
    * ✅ [Test] codecov 퍼센티지 적용
    
    * ✅ [Test] codecov patch test
    
    * ✅ [Test] codecov 적용완료
    
    ---------
    
    Co-authored-by: taehyeon <kth0373@naver.com>

--- potatocountry/src/main/java/com/potatocountry/potatocountry/PotatocountryApplication.java
+++ potatocountry/src/main/java/com/potatocountry/potatocountry/PotatocountryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PotatocountryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("args = " + args);
 		SpringApplication.run(PotatocountryApplication.class, args);
 	}
 
