ddf91ada733c48855075dcc4705261b05de1eff7
wonie <wonies@github.com>
2025-02-18 18:18:06 +0900

    security

--- potatocountry/src/main/java/com/potatocountry/potatocountry/domain/user/controller/UserController.java
+++ potatocountry/src/main/java/com/potatocountry/potatocountry/domain/user/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.potatocountry.potatocountry.domain.user.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping
 public class UserController {
 }
